1302828a36c1ad42f9f97af7ab494b5ed0111349
mtd <mtd@mpi-inf.mpg.de>
2017-03-30 07:04:25 +0200

    Added logging for info and debug.

--- logging/neuron_activity/logging_server.py
+++ logging/neuron_activity/logging_server.py
@@ -1,5 +1,5 @@
 import socket, SocketServer
-import argparse
+import argparse, logging
 import pickle,datetime
 import pandas as pd
 import numpy as np
@@ -60,28 +60,56 @@ class DataUDPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
     
         data = self.read_pickle_stream()   
-        
+        print data
+        logging.debug('reading packet: %s',data)
+
         self.process_row(data, hdf5_key = "address", cache = self.server.cache, max_cache_size = self.server.max_cache_size)
 
+
 # get the current timestamp and turn it into a string with second precision
-now = pd.Timestamp(np.datetime64(datetime.datetime.now())).strftime("%Y-%m-%d_%H:%M:%S")
+# now = pd.Timestamp(np.datetime64(datetime.datetime.now())).strftime("%Y-%m-%d_%H:%M:%S")
+now = "now"
 
 # setup parser for command line arguments
-parser = argparse.ArgumentParser(description='Real-time data h5 network data logging.', epilog='And this is how you log your data.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser = argparse.ArgumentParser(description='Real-time h5 network data storage', epilog='And this is how you log your data.', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 parser.add_argument('-s', '--sender_ip', metavar="", help='IP address of sending machine', type=str, default = socket.gethostname() )
 parser.add_argument('-p','--port', metavar="", help='port to listen to on sending machine', type=int, default = 6000)
-parser.add_argument('-f', '--output_file', metavar="", help="file name for h5 storage", type=str, default = "h5_store_" + now +".h5"   )
-parser.add_argument('-c','--cache_size', metavar="", help='what for this number of data entries to be recieved before flushing cache to h5 store', type=int, default = 10)
-parser.add_argument('-l', "--loglevel", metavar="", help="set loglevel for logger", type=int, default=0, choices=[0, 1, 2] )
+parser.add_argument('-f', '--h5_file', metavar="", help="file name for h5 storage", type=str, default = "h5_store_" + now +".h5"   )
+parser.add_argument('-c','--cache_size', metavar="", help='wait for this number of data entries to be recieved before flushing cache to h5 store', type=int, default = 10)
+parser.add_argument('-l', '--log_file', metavar="", help="file name for logs", type=str, default = "h5_store_" + now +".log"   )
+
 
 args = parser.parse_args()
 
-# Create the server, binding it to host and port
+# setup logging to file
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s: %(name)s: -- %(levelname)s -- %(message)s',
+                    filename=args.log_file,
+                    filemode='w')
+
+
+# define a Handler which writes INFO messages or higher to the sys.stderr
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)s: -- %(levelname)s -- %(message)s')
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
+
+logging.info('Starting real-time network data logging ...')
+logging.info('Configuring server ...')
+logging.info('Listen to ip address: %s', args.sender_ip )
+logging.info('Listen to port: %s', args.port )
+logging.info('Server cache size: %s', args.cache_size)
+logging.info('Arriving data is logged to h5 store: %s', args.h5_file)
+logging.info('Log files is specified as: %s', args.log_file)
+
 server = SocketServer.UDPServer((args.sender_ip,args.port), DataUDPHandler)
 server.cache = defaultdict(list)
 server.max_cache_size = args.cache_size
-server.hdf_store = pd.HDFStore(args.output_file, "w")
+server.hdf_store = pd.HDFStore(args.h5_file, "w")
 
-# print "Plot Server is listening ..."
+logging.info('Server ready.')
 server.serve_forever()
